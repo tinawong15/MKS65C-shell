bdbaac1b9a2956af870fc2d98356b775d8fd23ed
tina <tinaw425@gmail.com>
2018-12-01 22:29:15 -0500

    pipe??

--- shell.c
+++ shell.c
@@ -150,19 +150,21 @@ void redirect_stdin(char ** arr, int index) {
 
 void piping(char ** arr, int index) {
   int fds[2];
+  printf("| ?: %s\n", arr[index]);
   if(pipe(fds) == -1) {
     printf("pipe error: %s\n", strerror(errno));
   }
   int f = fork();
   if(f) {
     close(fds[READ]);
-  //   dup2(fds[WRITE], STDOUT_FILENO);
-  //   execvp(arr[0], arr);
+    dup2(fds[WRITE], STDOUT_FILENO);
+    arr[index] = NULL;
+    execvp(arr[0], arr);
   }
   else {
     close(fds[WRITE]);
-  //   dup2(fds[READ], STDIN_FILENO);
-  //   execvp(arr[0], arr);
+    dup2(fds[READ], STDIN_FILENO);
+    arr[index] = NULL;
+    execvp(arr[0], arr);
   }
-  return;
 }
